2da87be05b3e35e02f0448cb01f4bd0e1cd51051
lgyong <lgyong511@163.com>
2024-03-30 12:09:13 +0800

    写测试和更新README

--- logcuting_test.go
+++ logcuting_test.go
@@ -13,7 +13,7 @@ func TestNew(t *testing.T) {
 		Time: 0,
 		Size: 10,
 	})
-	t.Log("success, NewLogcuting=", l)
+	t.Log("new success, NewLogcuting=", l)
 }
 
 func TestWriter(t *testing.T) {
@@ -32,4 +32,31 @@ func TestCloser(t *testing.T) {
 		Size: 10,
 	})
 	_ = c
+}
+
+func TestUpdateConfig(t *testing.T) {
+	l := logcuting.NewLogcuting(&logcuting.Config{
+		Name: "./logs/demo-%Y%m%d%H%M.log",
+		Time: 0,
+		Size: 0,
+	})
+	t.Log("new success, NewLogcuting=", l)
+	l.UpdateConfig(&logcuting.Config{
+		Name: "./logs/demo-%Y-%m-%d-%H-%M.log",
+		Time: 10,
+		Size: 10,
+	})
+	t.Log("UpdateConfig success, UpdateConfig NewLogcuting=", l)
+}
+
+func BenchmarkWrite(b *testing.B) {
+	l := logcuting.NewLogcuting(&logcuting.Config{
+		Name: "./logs/demo-%Y%m%d%H%M.log",
+		Time: 0,
+		Size: 0,
+	})
+	s := `{"file":"E:/Myz-Nas/学习笔记/go编程语言/go-base/loging-test/main.go:43","func":"main.logConfig","level":"debug","msg":"使用配置文件创建的Debug","time":""}`
+	for i := 0; i < b.N; i++ {
+		l.Write([]byte(s))
+	}
 }
